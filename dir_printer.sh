103dad6c7e4a66105165a2155b196d159f3a3b54
Brendan Nolan <bnolan23@gmail.com>
2023-01-22 16:13:16 +0000

    Add Dir Explorer

---
+++ dir_printer.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+dirs=$(find . -maxdepth 1 -type d -not -path '*/\.*' -not -path '.')
+
+for dir in $dirs; do
+    cd $dir
+    echo "dir:"
+    echo $dir
+    dir_name_no_path=$(basename "$dir")
+    echo "dir without path:"
+    echo $dir_name_no_path
+    cd ..
+done
+
