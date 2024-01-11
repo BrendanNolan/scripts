8390a9ffd3dca670669533a461ddee043ce3db80
Brendan Nolan <bnolan23@gmail.com>
2024-01-11 08:11:48 +0000

    Fix Error

--- cht.sh
+++ cht.sh
@@ -7,7 +7,7 @@ core_utils=`echo "grep rg find fd sed awk" | tr ' ' '\n'`
 selected=`printf "$languages\n$core_utils" | fzf`
 read -p "query: " query
 
-if printf $languages | grep -qs $selected; then
+if echo "$languages" | grep -qa "$selected"; then
     tmux neww bash -c "curl cht.sh/$selected/`echo $query | tr ' ' '+'` & while [ : ]; do sleep 1; done"
 else
     tmux neww bash -c "curl cht.sh/$selected~$query & while [ : ]; do sleep 1; done"
