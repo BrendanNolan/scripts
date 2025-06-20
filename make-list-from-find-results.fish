098315349308f16a8f9e7d7567ab37b529bd6252
Brendan Nolan <bnolan23@gmail.com>
2025-06-20 07:08:37 +0100

    Remove unused

--- make-list-from-find-results.fish
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env fish
-
-set -l files (fd ".*sh" .)
-for f in $files
-    echo $f
-end
--- make-list-from-find-results.sh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env bash
-
-items=(`find . -name '*lua'`)
-for item in "${items[@]}"; do
-  echo "$item"
-done
