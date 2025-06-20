69181d3656e9ccbb589184cd35fa3f469547b9c7
Brendan Nolan <bnolan23@gmail.com>
2025-06-20 07:05:44 +0100

    Remove old syntax

--- cht.sh
+++ cht.sh
@@ -1,14 +1,14 @@
 #! /usr/bin/env bash
 # Credit The Primeagen
 
-languages=`echo "lua cpp c rust python" | tr ' ' '\n'`
-core_utils=`echo "grep rg find fd sed awk" | tr ' ' '\n'`
+languages=$(echo "lua cpp c rust python" | tr ' ' '\n')
+core_utils=$(echo "grep rg find fd sed awk" | tr ' ' '\n')
 
-selected=`printf "$languages\n$core_utils" | fzf`
+selected=$(printf "$languages\n$core_utils" | fzf)
 read -p "query: " query
 
 if echo "$languages" | grep -qa "$selected"; then
-    tmux neww bash -c "curl cht.sh/$selected/`echo $query | tr ' ' '+'` & while [ : ]; do sleep 1; done"
+    tmux neww bash -c "curl cht.sh/$selected/$(echo $query | tr ' ' '+') & while [ : ]; do sleep 1; done"
 else
     tmux neww bash -c "curl cht.sh/$selected~$query & while [ : ]; do sleep 1; done"
 fi
